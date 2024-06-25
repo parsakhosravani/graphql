459908e228869a93be52d0bdfd9a4e2f81bd8ee3
parsakhosravani <parsakhosravani@gmail.com>
2024-06-25 17:41:51 +0330

    🎉 init react apollo client app

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import "./App.css";
+
+import { useQuery, gql } from "@apollo/client";
+
+type TGetLocations = {
+  id: number;
+  name: string;
+  description: string;
+  photo: string;
+};
+const GET_LOCATIONS = gql`
+  query GetLocations {
+    locations {
+      id
+      name
+      description
+      photo
+    }
+  }
+`;
+function DisplayLocations() {
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return data.locations.map(
+    ({ id, name, description, photo }: TGetLocations) => (
+      <div key={id}>
+        <h3>{name}</h3>
+        <img
+          width="400"
+          height="250"
+          alt="location-reference"
+          src={`${photo}`}
+        />
+        <br />
+        <b>About this location:</b>
+        <p>{description}</p>
+        <br />
+      </div>
+    )
+  );
+}
+
+export default function App() {
+  return (
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <br />
+      <DisplayLocations />
+    </div>
+  );
+}
